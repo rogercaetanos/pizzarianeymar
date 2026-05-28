c89cd3ec0382110f783df328ef376898321efe5a
Rogério Caetano <rogercaetanos@gmail.com>
2026-05-28 10:54:33 +0000

    Mensagem start servidor tomcat

--- src/main/java/com/itb/inf2fm/pizzarianeymar/PizzarianeymarApplication.java
+++ src/main/java/com/itb/inf2fm/pizzarianeymar/PizzarianeymarApplication.java
@@ -8,6 +8,7 @@ public class PizzarianeymarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzarianeymarApplication.class, args);
+		System.out.println("Servidor rodando na porta 8080");
 	}
 
 }
