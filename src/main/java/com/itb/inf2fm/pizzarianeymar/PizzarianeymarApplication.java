7e2112c3810ad571bd78a54ae86a0bee4dc2c7cb
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-05-28 07:34:36 -0300

    Criado ambiente de Dev

---
+++ src/main/java/com/itb/inf2fm/pizzarianeymar/PizzarianeymarApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzarianeymar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarianeymarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzarianeymarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2fm/pizzarianeymar/PizzarianeymarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzarianeymar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzarianeymarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
